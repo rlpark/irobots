c21d0d754cd0ba04dfe5608977a001bfb463949e
Thomas <thomas.degris@inria.fr>
2012-07-19 08:48:54 +0200

    Put the robot in safe mode before playing

--- rlpark.plugin.irobot/src/rlpark/plugin/irobot/examples/CreateMusicClient.java
+++ rlpark.plugin.irobot/src/rlpark/plugin/irobot/examples/CreateMusicClient.java
@@ -6,11 +6,12 @@ import rlpark.plugin.irobot.robots.IRobotEnvironment;
 
 public class CreateMusicClient {
   public static void main(String[] args) {
-    IRobotEnvironment environment = new CreateRobot();
-    environment.waitNewObs();
-    environment.sendLeds(0, 255, true, true);
+    IRobotEnvironment robot = new CreateRobot();
+    robot.safeMode();
+    robot.waitNewObs();
+    robot.sendLeds(0, 255, true, true);
     while (true) {
-      environment.playSong(IRobotSongs.composeSadSong());
+      robot.playSong(IRobotSongs.DarthVador);
       try {
         Thread.sleep(10000);
       } catch (InterruptedException e) {
